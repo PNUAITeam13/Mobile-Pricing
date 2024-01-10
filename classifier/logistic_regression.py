a38ffc9785c53e91ddcb1fd176eb1cba67eb8f96
Anastasiia Tsapko <anastsapko@gmail.com>
2024-01-10 23:02:44 +0200

    [STARTAI-46] Small changes

--- classifier/logistic_regression.py
+++ classifier/logistic_regression.py
@@ -1,41 +1,41 @@
 import numpy as np
 
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
 class LogisticRegression:
-    
     def __init__(self, learning_rate=0.001, n_iters=1000):
-        self.learning_rate = learning_rate
-        self.n_iters = n_iters
-        self.classifiers = {}
+        self.__learning_rate = learning_rate
+        self.__n_iters = n_iters
+        self.__classifiers = {}
 
     def fit(self, X, y):
-        unique_classes = np.unique(y)
-        for cls in unique_classes:
+        for cls in np.unique(y):
             binary_y = np.where(y == cls, 1, 0)
-            self.classifiers[cls] = self.train_classifier(X, binary_y)
+            self.__classifiers[cls] = self.__train_classifier(X, binary_y)
 
-    def train_classifier(self, X, y):
+    def __train_classifier(self, X, y):
         n_samples, n_features = X.shape
         weights = np.zeros(n_features)
         bias = 0
 
-        for _ in range(self.n_iters):
+        for _ in range(self.__n_iters):
             linear_pred = np.dot(X, weights) + bias
             predictions = sigmoid(linear_pred)
 
             dw = (1 / n_samples) * np.dot(X.T, (predictions - y))
             db = (1 / n_samples) * np.sum(predictions - y)
 
-            weights = weights - self.learning_rate * dw
-            bias = bias - self.learning_rate * db
+            weights = weights - self.__learning_rate * dw
+            bias = bias - self.__learning_rate * db
 
         return weights, bias
 
     def predict(self, X):
         class_preds = []
-        for cls, classifier in self.classifiers.items():
+        for cls, classifier in self.__classifiers.items():
             weights, bias = classifier
             linear_pred = np.dot(X, weights) + bias
             y_pred = sigmoid(linear_pred)
